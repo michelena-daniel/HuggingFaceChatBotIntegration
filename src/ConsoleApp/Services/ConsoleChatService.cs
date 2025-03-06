8c4aa82e2fb30fb53b9d142949a3d6fd9e80f916
Daniel Romero <danielromero.sem@gmail.com>
2025-03-06 20:18:38 +0100

    Modified template project to use Hugging Face instead of OpenAI

---
+++ src/ConsoleApp/Services/ConsoleChatService.cs
@@ -0,0 +1,30 @@
+﻿using HuggingFaceChatApp.src.Application.Interfaces;
+
+namespace HuggingFaceChatApp.src.ConsoleApp.Services
+{
+    public class ConsoleChatService
+    {
+        private readonly IChatService _chatService;
+
+        public ConsoleChatService(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task RunAsync()
+        {
+            Console.WriteLine("Waking up Plant Care Bot");
+            while (true)
+            {
+                Console.Write("Enter your prompt: ");
+                string prompt = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(prompt))
+                    continue;
+                string response = await _chatService.GetChatResponseAsync(prompt);
+
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
